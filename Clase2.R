78e1fe8d50f70e4ee84a01cbd2644fc829845315
NataliaFV <n.fierr.vasquez@gmail.com>
2020-09-07 14:16:03 -0300

    En este paso se agrego el código para explorar plantas extintas

--- Clase2.R
+++ Clase2.R
@@ -1,3 +1,9 @@
 library(tidyverse)
 library(knitr)
 library(kableExtra)
+
+
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')
+
